1ce94a1ec765fe331c9bcd1e579f4aab10a73d45
supipis <70693604+supipis@users.noreply.github.com>
2021-05-05 14:09:44 +0200

    added s.korea

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 }
 
 function CountryList(props) {
-  let [countries, setCountries] = useState(['Sri Lanka', 'India', 'Pakistan', 'Nepal', 'Bhutan', 'Bangladesh', 'Maldives'])
+  let [countries, setCountries] = useState(['Sri Lanka', 'India', 'Pakistan', 'Nepal', 'Bhutan', 'Bangladesh', 'Maldives', 'South Korea'])
   return <ul id='country'>
 
     {countries.map(country => {
